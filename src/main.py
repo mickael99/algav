3000053e69b350706ccd093d67a3af8931971e65
mickael99 <mickael.temim99@gmail.com>
2022-11-22 19:25:44 +0100

    créationd de l'arbre de décision non compréssé (pas encore de commentaire et refactoring à faire)

--- src/main.py
+++ src/main.py
@@ -1,5 +1,8 @@
 #coding:utf-8
 
+from decision_tree import *
+from math import log2
+
 """
     Renvoie une liste de bits représentant la décomposition en base 2 de l’entier x,
     telle que les bits de poids les plus faibles soient présentés en tête de liste
@@ -53,8 +56,55 @@ def test_table():
     except AssertionError:
         print("Problème dans la fonction \"table\"")
 
+def test_power_of_2():
+    try:
+        assert True == is_power_of_2(64)
+        assert True == is_power_of_2(128)
+        assert True == is_power_of_2(4)
+        assert False == is_power_of_2(60)
+        assert False == is_power_of_2(100)
+        assert False == is_power_of_2(70)
+    except AssertionError:
+        print("Problème dans la fonction puissance de 2")
+
+def is_power_of_2(x):
+    if x == 1:
+        return True
+    if x < 1:
+        return False
+    return is_power_of_2(x / 2)
+
+index_build_tree = 0
+
+def rec_build_tree(l, t, level_max, current_level):
+    global index_build_tree
+    if current_level < level_max - 1:
+        t.insert_left("x" + str(level_max - current_level - 1))
+        t.insert_right("x" + str(level_max - current_level - 1))
+        rec_build_tree(l, t.left, level_max, current_level + 1)
+        rec_build_tree(l, t.right, level_max, current_level + 1)
+    else:
+        t.insert_left(str(l[index_build_tree]))
+        t.insert_right(str(l[index_build_tree + 1]))
+        index_build_tree += 2
+
+def build_tree(l):
+    while is_power_of_2(len(l)) == False:
+        l += [False]
+    logarithm_binary = int(log2(len(l)))
+    t = DecisionTree("x" + str(logarithm_binary))
+    index_build_tree = 0
+    rec_build_tree(l, t, logarithm_binary, 0)
+    return t
+
+#en developpement
+def luka(t):
+    return "(" + t.label + luka(t.left) + luka(t.right) + ")"
 
 if __name__ == "__main__":
     test_decomposition()
     test_completion()
     test_table()
+    test_power_of_2()
+
+print(build_tree(table(38, 8)))
